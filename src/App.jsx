e8c58faffc50bb71e89c978ba37d6fc813f25309
José Marinho <clowdcap@hotmail.com>
2022-10-25 00:19:06 -0300

    update setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+/* Import Setup inicial */
+import React from 'react'
+import './App.scss' 
+
+
+/* Import Material Ui */
+
+  /* Roboto Font */
+  import '@fontsource/roboto/300.css'
+  import '@fontsource/roboto/400.css'
+  import '@fontsource/roboto/500.css'
+  import '@fontsource/roboto/700.css'
+
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ src/components/contato/Contato.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './contato.scss'
+
+const Contato = () => {
+  return (
+    <div>Contato</div>
+  )
+}
+
+export default Contato
